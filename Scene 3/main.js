e87dccb248a78f7260e3a73b4ebcde7ade35b8f2
mathewjo99 <31700508+mathewjo99@users.noreply.github.com>
2020-02-14 14:57:43 +0530

    scene 3 initial

---
+++ Scene 3/main.js
@@ -0,0 +1,15 @@
+anime({
+    targets: '#City>g>path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInCubic',
+    delay:2000,
+    duration: 3000,
+    strokeWidth:2
+  });
+anime({
+    targets: '.svg-container-main',
+    easing: 'easeInOutQuad',
+    delay:2800,
+    duration: 3000,
+    opacity:1
+});  
