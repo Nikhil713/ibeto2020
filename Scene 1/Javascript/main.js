18e7e560230db57597494d3965a43b6e1cc1e5ca
mathewjo99 <31700508+mathewjo99@users.noreply.github.com>
2020-02-22 00:26:20 +0530

    scene 1-2-3 created

--- Scene 1/Javascript/main.js
+++ Scene 1/Javascript/main.js
@@ -1,7 +1,6 @@
-//---------------------------------ANIMATION OF SCENE 1: INTRO----------------------------------
-//---------------------------------------------------------------------------------------------
+
 anime({
-    targets: ".image-stroke",
+    targets: ".IMAGINE-TEXT-1",
     strokeDashoffset: [anime.setDashoffset, 0],
     easing: 'easeInOutSine',
     duration: 1000,
@@ -9,7 +8,7 @@ anime({
   });//animation of image stroke dashoffset
 
 anime({
-    targets: ".image-stroke",
+    targets: ".IMAGINE-TEXT-1",
     duration: 900,
     delay:2500,
     easing: 'easeInOutSine',
@@ -25,80 +24,146 @@ anime({
  });//animation of white background to transparent background
 
  anime({
-    targets: ".svg-container",
-    duration: 600,
-    delay:2500,
+    targets: ".night-sky-container",
     easing: 'easeInOutSine',
-    opacity:1
+    opacity:[
+      { value: 1, duration: 600, delay: 2500 },
+      { value: 0, duration: 1600, delay: 7200 }
+    ]
  });//animation of revealing universe 
 
  anime({
-    targets: ".scene-1-text",
+    targets: ".scene-text",
     duration: 600,
     delay:4000,
     easing: 'easeInOutSine',
-    opacity:1
+    opacity:[
+      { value: 1, duration: 600, delay: 4000 },
+      { value: 0, duration: 600, delay: 5200 }
+    ]
  });//animation of revealing scene 1 tag line after the scene sets
 
 //------------------------------ANIMATION OF SCENE 1: COMETS ----------------------------------
 //---------------------------------------------------------------------------------------------
 
  anime({
-    targets: "#Comet_1",
-    duration: 1000,
-    delay:4200,
+    targets: "#Comet-1",
+    duration: 2000,
+    delay:200,
     easing: 'linear',
-    translateX: [200,-1000],
-    translateY: [-200,1000]
+    translateX: [1000,-1000],
+    translateY: [-700,700],
+    loop:true
  });//animation of comet 
  
  anime({
-    targets: "#Comet_2",
-    duration: 1000,
-    delay:5400,
-    easing: 'linear',
-    translateX: [200,-1000],
-    translateY: [-200,1000]
- });//animation of comet 
-
- anime({
-    targets: "#Comet_3",
-    duration: 1000,
-    delay:6600,
-    easing: 'linear',
-    translateX: [200,-1000],
-    translateY: [-200,1000]
- });//animation of comet 
-
- anime({
-    targets: "#Comet_4",
-    duration: 1000,
-    delay:7800,
-    easing: 'linear',
-    translateX: [200,-1000],
-    translateY: [-200,1000]
- });//animation of comet 
-
- anime({
-    targets: "#Comet_5",
-    duration: 1000,
-    delay:9000,
-    easing: 'linear',
-    translateX: [200,-1000],
-    translateY: [-200,1000]
- });//animation of comet 
-
-//--------------------ANIMATION OF SCENE 1: ORBIT AROUND BROWN PLANET -------------------------
-//---------------------------------------------------------------------------------------------
-
+   targets: "#Comet-2",
+   duration: 2000,
+   delay:500,
+   easing: 'linear',
+   translateX: [1000,-1000],
+   translateY: [-700,700],
+   loop:true
+});//animation of comet 
 
 anime({
-    targets: "#Orbit_of_brown_planet",
-    duration: 9000,
-    delay:2200,
-    easing: 'linear',
-    scale:1.3
- });//animation of orbit around brown planet
+   targets: "#Comet-3",
+   duration: 2000,
+   delay:900,
+   easing: 'linear',
+   translateX: [1000,-1000],
+   translateY: [-700,700],
+   loop:true
+});//animation of comet 
 
-//---------------------------ANIMATION OF SCENE 1: RED PLANET ---------------------------------
-//---------------------------------------------------------------------------------------------
+anime({
+   targets: "#Comet-4",
+   duration: 2000,
+   delay:900,
+   easing: 'linear',
+   translateX: [1000,-1000],
+   translateY: [-700,700],
+   loop:true
+});//animation of comet 
+
+anime({
+   targets: "#Comet-5",
+   duration: 2000,
+   delay:1200,
+   easing: 'linear',
+   translateX: [1000,-1000],
+   translateY: [-700,700],
+   loop:true
+});//animation of comet 
+
+anime({
+   targets: "#Comet-6",
+   duration: 2000,
+   delay:1500,
+   easing: 'linear',
+   translateX: [1000,-1000],
+   translateY: [-700,700],
+   loop:true
+});//animation of comet 
+
+anime({
+   targets: "#Comet-7",
+   duration: 2000,
+   delay:2200,
+   easing: 'linear',
+   translateX: [1000,-1000],
+   translateY: [-700,700],
+   loop:true
+});//animation of comet 
+
+anime({
+   targets: "#Comet-8",
+   duration: 2000,
+   delay:2200,
+   easing: 'linear',
+   translateX: [1000,-1000],
+   translateY: [-700,700],
+   loop:true
+});//animation of comet 
+
+anime({
+   targets: "#Comet-9",
+   duration: 2000,
+   delay:2900,
+   easing: 'linear',
+   translateX: [1000,-1000],
+   translateY: [-700,700],
+   loop:true
+});//animation of comet 
+
+anime({
+   targets: "#Comet-10",
+   duration: 2000,
+   delay:3100,
+   easing: 'linear',
+   translateX: [1000,-1000],
+   translateY: [-700,700],
+   loop:true
+});//animation of comet 
+
+anime({
+   targets: ".NIGHT-SKY-3",
+   duration: 1000,
+   easing: 'linear',
+   opacity:[0,1],
+   direction:'alternate',
+   loop:true
+});//animation of comet 
+
+anime({
+   targets:[".turbulence-3",".displacementmap-3"],
+   baseFrequency: 0.04,
+   scale: 20,
+   numOctave:10,
+   duration: 4500,
+   easing:'easeInQuad',
+   delay:7000
+})
+// setTimeout(function(){
+//    document.getElementById('displacementMapScale').setAttribute("scale","20")
+// },7000)
