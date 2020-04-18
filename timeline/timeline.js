6d4d70232a7bc6773894c680f4050556b87bff53
VaishnavMenon <menonvaishnavs@gmail.com>
2020-04-18 12:40:32 +0530

    corrected timeline-divs disappearing all at once

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -2,9 +2,9 @@
 function isElementInViewport(el) {
   var rect = el.getBoundingClientRect();
   return (
-    rect.top >= 0 &&
+    rect.top >=0 &&
     rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)*0.5&&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
