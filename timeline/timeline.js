5e74e1bb4768bc09faa8cf8c0f56a2d0c899dc8f
VaishnavMenon <menonvaishnavs@gmail.com>
2020-04-19 09:49:52 +0530

    added media query for timeline

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -4,7 +4,7 @@ function isElementInViewport(el) {
   return (
     rect.top >=0 &&
     rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)*0.5&&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)*0.75&&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
@@ -32,7 +32,7 @@ window.addEventListener('scroll', function(){
     {
       for (var i = 0; i < items.length; i++) {
         if (isElementInViewport(items[i])) {
-        items[i].classList.remove("in-view");
+       // items[i].classList.remove("in-view");
         }
       }
     }
