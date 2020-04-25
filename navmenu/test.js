f7021d074c5a7c16d17ea728e465d984a4b34930
VaishnavMenon <menonvaishnavs@gmail.com>
2020-04-25 08:44:15 +0530

    fixed navbar error(test.html)

---
+++ navmenu/test.js
@@ -0,0 +1,28 @@
+var $els = $('.menu a, .menu header');
+        var count = $els.length;
+        var grouplength = Math.ceil(count/3);
+        var groupNumber = 0;
+        var i = 1;
+        $('.menu').css('--count',count+'');
+        $els.each(function(j){
+            if ( i > grouplength ) {
+                groupNumber++;
+                i=1;
+            }
+            $(this).attr('data-group',groupNumber);
+            i++;
+        });
+
+        $('.menu footer button').on('click',function(e){
+            e.preventDefault();
+            $els.each(function(j){
+                $(this).css('--top',$(this)[0].getBoundingClientRect().top + ($(this).attr('data-group') * -15) - 20);
+                $(this).css('--delay-in',j*.1+'s');
+                $(this).css('--delay-out',(count-j)*.1+'s');
+                $(this).css('--left',$(this)[0].getBoundingClientRect().left + (-1800));
+            });
+            $('.menu').toggleClass('closed');
+            e.stopPropagation();
+        });
+
+        
